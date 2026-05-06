efdd6350b93f4ba5427926556e96d20fd2e92788
Murilo Possamai <murilo.p2005@aluno.ifsc.edu.br>
2026-05-06 15:20:00 -0300

    configurei main com tema e rotas nomeadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/tarefa_provider.dart';
+import 'screens/boas_vindas_screen.dart';
+import 'screens/lista_screen.dart';
+import 'screens/detalhes_screen.dart';
+import 'screens/inserir_screen.dart';
+import 'screens/editar_screen.dart';
+import 'models/tarefa.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => TarefaProvider(),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +23,55 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Tarefas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFF000000),
+          secondary: Color(0xFFFF3B30),
+          surface: Color(0xFFF5F5F5),
         ),
+        scaffoldBackgroundColor: const Color(0xFFFFFFFF),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFFFFFFFF),
+          foregroundColor: Color(0xFF000000),
+          elevation: 0,
+          scrolledUnderElevation: 0,
+          titleTextStyle: TextStyle(
+            color: Color(0xFF000000),
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Color(0xFF000000)),
+          bodySmall: TextStyle(color: Color(0xFF757575)),
+        ),
+        dividerColor: Color(0xFFE0E0E0),
+        useMaterial3: true,
       ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const BoasVindasScreen(),
+        '/lista': (context) => const ListaScreen(),
+        '/inserir': (context) => const InserirScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/detalhes') {
+          final tarefa = settings.arguments as Tarefa;
+          return MaterialPageRoute(
+            builder: (_) => DetalhesScreen(tarefa: tarefa),
+          );
+        }
+        if (settings.name == '/editar') {
+          final tarefa = settings.arguments as Tarefa;
+          return MaterialPageRoute(
+            builder: (_) => EditarScreen(tarefa: tarefa),
+          );
+        }
+        return null;
+      },
     );
   }
 }
